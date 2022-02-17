bf0220c5f772cd25bf0063ffc7c0e5f70bfd6d6f
GDW1 <guywilks@ucsb.edu>
2022-02-16 21:07:25 -0800

    sandbox.c

--- demos/Sandboxing/sandboxing.c
+++ demos/Sandboxing/sandboxing.c
@@ -1,16 +1,23 @@
-//
-// Created by Guy Wilks on 2/16/22.
-//
 
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include "config.h"
-#include "seccomp-bpf.h"
-
-
+#define _GNU_SOURCE 1 
+#include <sys/sendfile.h> 
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <sys/mman.h> 
+#include <seccomp.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <stdint.h> 
+#include <assert.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <errno.h> 
+#include <fontl.h> 
+#include <time.h>
 int main(int argc, char **argv){
-    scmp_filter_ctx ctx;
+	scmp_filter_ctx ctx;
+	ctx = seccomp_init(SCMP_ACT_ALLOW);
+	seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 0);
+	seccomp_load(ctx);
+	execl("/bin/cat", "cat", "/flag", (char *)0);
 }
